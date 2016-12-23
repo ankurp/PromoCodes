fdaabbf3a7f2e1aaf8d3345008aa93fc1df8bc74
Ankur Patel <ankur.patel@ymail.com>
2016-12-23 02:48:30 -0500

    Updating order.js to redirect correctly

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -2,6 +2,7 @@ $(function() {
     $(".promocode-link").on("click", function(event) {
         event.preventDefault();
         var code = $("#promocode").val();
-        window.location = window.location + "?promocode=" + code;
+        var redirecUrl = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + window.location.pathname + "?promocode=" + code;
+        window.location = redirecUrl;
     });
 });
